17e30f6c137f8ca1141008390471209feb8504ff
Oscar <gumball_junior@hotmail.com>
2021-09-29 10:35:54 -0500

    second commit add functions like update, create, read and delete

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.models.query_utils import select_related_descend
 from django.test import Client, TestCase
 from django.urls import reverse
 from .models import Post
@@ -24,6 +25,9 @@ class BlogTests(TestCase):
         post = Post(title='A sample title')
         self.assertEqual(str(post), post.title)
 
+    def test_get_absolute_url(self):
+        self.assertEqual(self.get_absolute_url(), '/post/1/')
+
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
         self.assertEqual(f'{self.post.author}', 'testuser')
@@ -42,3 +46,26 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user,
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Update title',
+            'body': 'Update text'
+        })
+
+        self.assertEqual(response.status_code, 302)
+    
+    def test_post_delete_view(self):
+        response = self.client.get(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 200)
